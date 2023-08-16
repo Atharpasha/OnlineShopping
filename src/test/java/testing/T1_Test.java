5c4f501b5cd3dc0731018a0cb268df0e84453dd4
Athar <Athar@laptop>
2023-08-16 20:45:30 +0530

    project is added to local repo

---
+++ src/test/java/testing/T1_Test.java
@@ -0,0 +1,26 @@
+package testing;
+
+import org.testng.annotations.Test;
+
+public class T1_Test  {
+	@Test
+	public void create()
+	{
+		System.out.println("--create--");	
+	}
+
+	@Test
+	public void edit() 
+	{
+		System.out.println("--edit--");
+	}
+
+	@Test
+	public void delete()
+	{
+		System.out.println("delete");	
+	}
+	
+
+
+}
